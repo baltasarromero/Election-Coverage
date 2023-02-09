a187308655a6168e2b82853ccfd576dd81815011
baltasarromero <baltasarromero@gmail.com>
2023-02-09 10:48:55 -0300

    Added more tests to increase coverage. Updated tests file name

--- test/Election.ts
+++ test/Election.ts
@@ -2,10 +2,12 @@ import { USElection__factory } from "./../typechain-types/factories/Election.sol
 import { USElection } from "./../typechain-types/Election.sol/USElection";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Signer } from "ethers";
 
 describe("USElection", function () {
   let usElectionFactory;
   let usElection: USElection;
+  let addr1: Signer;
 
   before(async () => {
     usElectionFactory = await ethers.getContractFactory("USElection");
@@ -13,6 +15,7 @@ describe("USElection", function () {
     usElection = await usElectionFactory.deploy();
 
     await usElection.deployed();
+    [, addr1] = await ethers.getSigners();
   });
 
   it("Should return the current leader before submit any election results", async function () {
@@ -35,7 +38,7 @@ describe("USElection", function () {
     expect(await usElection.currentLeader()).to.equal(1); // BIDEN
   });
 
-  it("Should throw when try to submit already submitted state results", async function () {
+  it("Should throw when tryng to submit already submitted state results", async function () {
     const stateResults = ["California", 1000, 900, 32];
 
     expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
@@ -44,7 +47,7 @@ describe("USElection", function () {
   });
 
   it("Should submit state results and get current leader", async function () {
-    const stateResults = ["Ohaio", 800, 1200, 33];
+    const stateResults = ["Ohio", 800, 1200, 33];
 
     const submitStateResultsTx = await usElection.submitStateResult(
       stateResults
@@ -53,6 +56,31 @@ describe("USElection", function () {
     await submitStateResultsTx.wait();
 
     expect(await usElection.currentLeader()).to.equal(2); // TRUMP
+  });
+
+  it("Should throw when seats not submitted", async function () {
+    const stateResults = ["Nevada", 780, 500, 0];
+    expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+      "States must have at least 1 seat"
+    );
+  });
+
+  it("Should throw when there is a tie", async function () {
+    const stateResults = ["Florida", 999, 999, 40];
+    expect(usElection.submitStateResult(stateResults)).to.be.revertedWith(
+      "There cannot be a tie"
+    );
+  });
+
+  it("Should throw when trying to submit results with an account different than the owner", async function () {
+    const stateResults = ["Illinois", 520, 518, 28];
+    expect(usElection.connect(addr1).submitStateResult(stateResults)).to.be.revertedWith('Ownable: Not invoked by the owner');
+  });
+
+
+  it("Should throw when trying to end election with an account different than the owner", async function () {
+    expect(usElection.connect(addr1).endElection()).to.be.revertedWith('Ownable: Not invoked by the owner');
+    expect(await usElection.electionEnded()).to.equal(false); // Not Ended
   });
 
   it("Should end the elections, get the leader and election status", async function () {
@@ -65,5 +93,9 @@ describe("USElection", function () {
     expect(await usElection.electionEnded()).to.equal(true); // Ended
   });
 
-  //TODO: ADD YOUR TESTS
+  it("Should revert if the election is already ended", async function () {
+    expect(usElection.endElection()).to.be.revertedWith(
+      "The election has ended already"
+    );
+  });
 });
